21e5a0142fb8a2d303883dc851c2ff8dccc674b9
roberto <roberto.marabini.uam.es>
2016-12-07 16:51:14 +0100

    add view and api mapping

--- pyworkflow/web/workflowarchive/urls.py
+++ pyworkflow/web/workflowarchive/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import patterns, url
-from workflowarchive import views
-from workflowarchive.api import WorkflowResource
+from django.conf.urls import patterns, url, include
+import views
+from api import WorkflowResource
+workflow_resource = WorkflowResource()
 
 urlpatterns = patterns('',
     url(r'^$', views.archive_workflow, name='archive_workflow'),
     url(r'^archive/$', views.archive_workflow, name='archive'),
     url(r'^list/$', views.list_workflow, name='list_workflow'),
+    url(r'download/(?P<workflow_id>\d+)/',views.download_workflow,name='download_workflow'),
+    url(r'^api/', include(workflow_resource.urls)),
 )
